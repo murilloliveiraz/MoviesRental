dc8d89f76c9d76dc1c728d4ff0fd95b48d0c48d0
Murillo Oliveira <murillo1047@gmail.com>
2025-01-03 13:44:29 -0300

    feat: write repositories context

---
+++ src/Services/Publisher/Infraestructure/MoviesRental.Infraestructure/Context/MoviesRentalWriteContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesRental.Domain.Entities;
+
+namespace MoviesRental.Infraestructure.Context
+{
+    public class MoviesRentalWriteContext : DbContext
+    {
+        public MoviesRentalWriteContext(){}
+
+        public MoviesRentalWriteContext(DbContextOptions<MoviesRentalWriteContext> options): base(options) { }
+
+        public DbSet<Director> Directors { get; set; }
+        public DbSet<Dvd> Dvds { get; set; }
+    }
+}
