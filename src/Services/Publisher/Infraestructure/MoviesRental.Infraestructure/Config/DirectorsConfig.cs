77e6025f991f271133ef1547fbd622c15895f43c
Murillo Oliveira <murillo1047@gmail.com>
2025-01-03 13:45:58 -0300

    feat: modeling write database

---
+++ src/Services/Publisher/Infraestructure/MoviesRental.Infraestructure/Config/DirectorsConfig.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoviesRental.Domain.Entities;
+
+namespace MoviesRental.Infraestructure.Config
+{
+    public class DirectorsConfig : IEntityTypeConfiguration<Director>
+    {
+        public void Configure(EntityTypeBuilder<Director> builder)
+        {
+            builder.ToTable("Directors");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(Director.MAX_LENGTH);
+
+            builder.Property(x => x.Surname)
+                .IsRequired()
+                .HasMaxLength(Director.MAX_LENGTH);
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+
+            builder.Property(x => x.UpdatedAt)
+                .IsRequired();
+
+            builder.HasMany(x => x.Dvds)
+                .WithOne(d => d.Director);
+        }
+    }
+}
---
+++ src/Services/Publisher/Infraestructure/MoviesRental.Infraestructure/Config/DvdsConfig.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoviesRental.Domain.Entities;
+
+namespace MoviesRental.Infraestructure.Config
+{
+    public class DvdsConfig : IEntityTypeConfiguration<Dvd>
+    {
+        public void Configure(EntityTypeBuilder<Dvd> builder)
+        {
+            builder.ToTable("Dvds");
+
+            builder.HasKey(x => x.Id);
+
+            builder.HasIndex(x => x.Title)
+                .IsUnique();
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(Dvd.MAX_TITLE_LENGTH);
+
+            builder.Property(x => x.Copies)
+                .IsRequired();
+
+            builder.Property(x => x.Available)
+                .IsRequired();
+
+            builder.Property(x => x.Published)
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+
+            builder.Property(x => x.UpdatedAt)
+                .IsRequired();
+
+            builder.Property(x => x.DirectorId)
+                .IsRequired();
+
+            builder.HasOne(x => x.Director)
+                .WithMany(d => d.Dvds)
+                .HasForeignKey(x => x.DirectorId);
+
+            builder.Property(x => x.Genre)
+                .HasConversion(v => v.ToString(), v => (EGenre)Enum.Parse(typeof(EGenre), v));
+        }
+    }
+}
