f84a4e8bc84207821ceeea36a92442a58a251ca7
Murillo Oliveira <murillo1047@gmail.com>
2025-01-03 07:44:14 -0300

    feat: validations messages to fluent validation

---
+++ src/BuildingBlocks/Core/ValidationMessages/ValidationMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesRental.Core.ValidationMessages
+{
+    public static class ValidationMessages
+    {
+        public const string MIN_LENGTH_ERROR_MESSAGE = "{PropertyName} must have at least {MinLength} characters";
+        public const string MAX_LENGTH_ERROR_MESSAGE = "{PropertyName} must not reach {MaxLength} characters";
+        public const string EMPTY_STRING_ERROR_MESSAGE = "{PropertyName} can't be empty";
+        public const string ERROR_MESSAGE = "Invalid {PropertyName}";
+    }
+}
