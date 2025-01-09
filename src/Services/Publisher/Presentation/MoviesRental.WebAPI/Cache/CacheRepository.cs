94a24b5c521a48b7623ff7b95af67c03a0a92828
Murillo Oliveira <murillo1047@gmail.com>
2025-01-09 10:09:59 -0300

    feat: redis cache implementation

---
+++ src/Services/Publisher/Presentation/MoviesRental.WebAPI/Cache/CacheRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using MoviesRental.Query.Application.Features.Dvds.Queries.GetDvd;
+using System.Text.Json;
+
+namespace MoviesRental.WebAPI.Cache
+{
+    public class CacheRepository : ICacheRepository
+    {
+        private readonly IDistributedCache _redisCache;
+        private readonly DistributedCacheEntryOptions _cacheEntryOptions;
+
+        public CacheRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+            _cacheEntryOptions = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(20)
+            };
+        }
+
+        public async Task<GetDvdResponse> Get(string title)
+        {
+            var response = await _redisCache.GetStringAsync(title);
+            if (response is null)
+                return default;
+            return JsonSerializer.Deserialize<GetDvdResponse>(response);
+        }
+
+        public async Task Update(GetDvdResponse response)
+        {
+            await _redisCache.SetStringAsync(response.Title, JsonSerializer.Serialize(response), _cacheEntryOptions);
+        }
+    }
+}
---
+++ src/Services/Publisher/Presentation/MoviesRental.WebAPI/Cache/ICacheRepository.cs
@@ -0,0 +1,10 @@
+﻿using MoviesRental.Query.Application.Features.Dvds.Queries.GetDvd;
+
+namespace MoviesRental.WebAPI.Cache
+{
+    public interface ICacheRepository
+    {
+        Task<GetDvdResponse> Get(string title);
+        Task Update(GetDvdResponse response);
+    }
+}
